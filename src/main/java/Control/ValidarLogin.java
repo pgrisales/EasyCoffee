2ba183caba1f6e457b51c460a1152270fdff7599
crvargasm <42346462+crvargasm@users.noreply.github.com>
2020-05-26 22:09:26 -0500

    Modificaciones a DAOManager

--- src/main/java/Control/ValidarLogin.java
+++ src/main/java/Control/ValidarLogin.java
@@ -1,13 +1,38 @@
 package Control;
 
+import DAO.DAOManager;
 import com.easycoffee.Usuario;
+import java.sql.SQLException;
 
 public class ValidarLogin {
 
+    DAO.DAOManager daoManager;
+
     public ValidarLogin() {
+        this.daoManager = new DAOManager("localhost:1527", "root", "admin", "easycoffedb");
     }
 
     public boolean verificarLogin(Usuario user) {
-        return false;
+        return compararUsername(user) && compararPassword(user);
     }
+
+    private boolean compararUsername(Usuario user) {
+        System.out.println(daoManager.getUsuarioDAO().obtener(user.getCedula()).getCedula());
+        return user.getUsername().equals(daoManager.getUsuarioDAO().obtener(user.getCedula()).getUsername());
+    }
+
+    private boolean compararPassword(Usuario user) {
+        return user.getPassword().equals(daoManager.getUsuarioDAO().obtener(user.getCedula()).getPassword());
+    }
+
+//    public static void main(String[] args) {
+//        Usuario test = new Usuario("admin3", "134", (long) 195, "Camilo", "Vargas", true);
+//        ValidarLogin login = new ValidarLogin();
+//        boolean logear = login.verificarLogin(test);
+//        if (logear) {
+//            System.out.println("Bienvenido " + test.getNombre());
+//        } else {
+//            System.out.println("Upsss.... Algo Ha salido mal :/");
+//        }
+//    }
 }
--- src/main/java/DAO/DAOManager.java
+++ src/main/java/DAO/DAOManager.java
@@ -21,53 +21,57 @@ public class DAOManager {
     private ArbolDAO arboles = null;
 
 //    database :  "jdbc:derby://localhost:1527/easycoffedb" Username: "root" Password: "admin"
-    public DAOManager(String host, String username, String password, String database) throws SQLException {
-        conn = DriverManager.getConnection("jdbc:derby://" + host + "/" + database, username, password);
+    public DAOManager(String host, String username, String password, String database) {
+        try {
+            conn = DriverManager.getConnection("jdbc:derby://" + host + "/" + database, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    UsuarioDAO getUsuarioDAO() {
+    public UsuarioDAO getUsuarioDAO() {
         if (usuarios == null) {
             usuarios = new UsuarioDAO(conn);
         }
         return usuarios;
     }
 
-    TrabajadorDAO getTrabajadorDAO() {
+    public TrabajadorDAO getTrabajadorDAO() {
         if (trabajadores == null) {
             trabajadores = new TrabajadorDAO(conn);
         }
         return trabajadores;
     }
 
-    PersonaDAO getPersonaDAO() {
+    public PersonaDAO getPersonaDAO() {
         if (personas == null) {
             personas = new PersonaDAO(conn);
         }
         return personas;
     }
 
-    LoteDAO getLoteDAO() {
+    public LoteDAO getLoteDAO() {
         if (lotes == null) {
             lotes = new LoteDAO(conn);
         }
         return lotes;
     }
 
-    JornadaDAO getJornadaDAO() {
+    public JornadaDAO getJornadaDAO() {
         if (jornadas == null) {
             jornadas = new JornadaDAO(conn);
         }
         return jornadas;
     }
 
-    FincaDAO getFincaDAO() {
+    public FincaDAO getFincaDAO() {
         if (fincas == null) {
             fincas = new FincaDAO(conn);
         }
         return fincas;
     }
 
-    ArbolDAO getArbolDAO() {
+    public ArbolDAO getArbolDAO() {
         if (arboles == null) {
             arboles = new ArbolDAO(conn);
         }
@@ -78,7 +82,7 @@ public class DAOManager {
         conn.close();
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         DAOManager DAO = new DAOManager("localhost:1527", "root", "admin", "easycoffedb");
         List<Persona> personas = DAO.getPersonaDAO().obtenerTodos();
         for (Persona p : personas) {
