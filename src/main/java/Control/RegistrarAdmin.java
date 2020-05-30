a578c034ba8b838686545082894a231a483ab602
Camilo Vargas <crvargasm@unal.edu.co>
2020-05-30 14:42:28 -0500

    Correccion Añadir Administrador

--- src/main/java/Control/RegistrarAdmin.java
+++ src/main/java/Control/RegistrarAdmin.java
@@ -7,6 +7,7 @@ package Control;
 
 import DAO.DAOManager;
 import com.easycoffee.Administrador;
+import com.easycoffee.Persona;
 import com.easycoffee.Usuario;
 import java.sql.SQLException;
 
@@ -15,13 +16,17 @@ import java.sql.SQLException;
  * @author Nivektakedown
  */
 public class RegistrarAdmin {
+
     DAO.DAOManager daoManager;
 
     public RegistrarAdmin() {
         this.daoManager = new DAOManager("localhost:1527", "root", "1234", "easycoffebd");
     }
-    public void registrar(Administrador admin) throws SQLException{
-        Usuario u= new Usuario(admin.getPassword(), admin.getCedula(), admin.getNombre(), admin.getApellido(), true, admin.getRespuesta()[0], admin.getRespuesta()[0], admin.getRespuesta()[0]);
+
+    public void registrar(Administrador admin) throws SQLException {
+        Persona p = new Persona(admin.getCedula(), admin.getNombre(), admin.getPassword(), true);
+        daoManager.getPersonaDAO().insertar(p);
+        Usuario u = new Usuario(admin.getPassword(), admin.getCedula(), admin.getNombre(), admin.getApellido(), true, admin.getRespuesta()[0], admin.getRespuesta()[0], admin.getRespuesta()[0]);
         daoManager.getUsuarioDAO().insertar(u);
         daoManager.cerrarConexion();
     }
