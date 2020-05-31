a9235db7923715f1e271eb8c22cd2ce1fafab005
Camilo Vargas <crvargasm@unal.edu.co>
2020-05-31 01:17:50 -0500

    Prueba Carga Café Realizada

--- src/test/java/PruebasPrimerNivel/CargaPrecioCafe.java
+++ src/test/java/PruebasPrimerNivel/CargaPrecioCafe.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PruebasPrimerNivel;
 
 import WebS.PrecioCafe;
@@ -20,7 +15,6 @@ import static org.junit.Assert.*;
  */
 public class CargaPrecioCafe {
 
-//    private static PrecioCafe precioCafe = PrecioCafe.getInstance();
     public CargaPrecioCafe() {
     }
 
